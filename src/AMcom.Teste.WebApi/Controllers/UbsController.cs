2f8285bf68e8db7115584bcfe740c006489d21e0
JefferssonSemin <jeffesemin@hotmail.com>
2019-10-16 08:16:19 -0300

    removido classes inutilizadas

--- src/AMcom.Teste.WebApi/Controllers/UbsController.cs
+++ src/AMcom.Teste.WebApi/Controllers/UbsController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using AMcom.Teste.Service.Dtos;
-using AMcom.Teste.Service.Interfaces;
+﻿using AMcom.Teste.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AMcom.Teste.WebApi.Controllers
@@ -8,7 +6,6 @@ namespace AMcom.Teste.WebApi.Controllers
     [Route("api/ubs")]
     public class UbsController : Controller
     {
-
         private readonly IUbsService _ubsService;
 
         public UbsController(IUbsService ubsService)
@@ -17,8 +14,7 @@ namespace AMcom.Teste.WebApi.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListaUbs() => Ok();
-
+        public IActionResult ListaUbs() => Ok("teste");
 
         // Implemente um método que seja acessível por HTTP GET e retorne a lista das 5 UBSs
         // (Unidades Básicas de Saúde) mas próximas de um ponto (latitude e longitude) e ordenada
